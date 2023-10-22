aa88b0f064980f3c054bd390e41b3aa570eff9a5
MostofaNobiTanveer <mntanveer9196@gmail.com>
2023-10-22 17:00:08 +0600

    wip

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,33 +4,28 @@ import { PiShareNetworkThin } from 'react-icons/pi';
 
 const Nav = () => {
   return (
-    <header className="h-screen">
-      <nav
-        class="container mx-auto flex items-center justify-between p-6 lg:px-8"
-        aria-label="Global"
-      >
-        <div class="flex flex-1">
-          <div class="hidden lg:flex lg:gap-x-12">
+    <header className="m-5 rounded-lg bg-white">
+      <nav className="container mx-auto flex items-center justify-between p-6 lg:px-8">
+        {/* <div className="flex flex-1">
+          <div className="hidden lg:flex lg:gap-x-12">
 
           </div>
-        </div>
-        {/* <Link to="/" class="-m-1.5 p-1.5 font-light text-3xl text-center tracking-[.3em]">
+        </div> */}
+        {/* <Link to="/" className="-m-1.5 p-1.5 font-light text-3xl text-center tracking-[.3em]">
           MOSTOFA<br />NOBI
         </Link> */}
         <Link to="/" class="font-extrabold text-3xl text-left leading-5 tracking-tighter">
           MOSTOFA<br />NOBI<span className="text-6xl leading-4 text-[#ff2c2c]">.</span>
         </Link>
-        <div class="flex flex-1 justify-end items-center gap-2">
-          {/* <span class="relative flex h-2 w-2 justify-center items-center">
-            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
-            <span class="relative inline-flex rounded-full h-1 w-1 bg-white"></span>
+        <Link to="/" className="flex items-center gap-2">
+          <span className="relative flex h-2 w-2 justify-center items-center">
+            <span className="animate-ping absolute h-full w-full rounded-full bg-orange opacity-75"></span>
+            <span className="rounded-full h-1 w-1 bg-orange"></span>
           </span>
-          <p className="font-light uppercase text-xs tracking-[.3em]">Social</p>
-          <div className='h-10 w-10 bg-gray-400 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 flex items-center justify-center'>
-            <PiShareNetworkThin />
-          </div> */}
-        </div>
+          <p className="uppercase">About</p>
+        </Link>
       </nav>
+      <h1 className="font-autograph text-5xl">Mostofa Nobi</h1>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ module.exports = {
     extend: {
       fontFamily: {
         kumbh: ["Kumbh Sans", "sans-serif"],
+        sora: ["Sora", "sans-serif"],
+        josefin: ["Josefin Sans", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
-      }
+        autograph: ["autograph", "sans-serif"],
+        graphit: ["graphit", "sans-serif"],
+        'space-grotesk': ['Space Grotesk', "sans-serif"]
+      },
+      colors: {
+        orange: '#F44E2B',
+        dune: '#3F3531',
+        pearl: '#FAF7EE'
+      },
     }
   },
   plugins: [],
